cd52ac24150b18c06414d8c0d77d6ee47786e766
Roberto Ramirez <robert.624@hotmail.com>
2022-12-22 11:24:10 -0500

    reto 21 another way

--- reto_21.js
+++ reto_21.js
@@ -1,36 +1,47 @@
+// funcion lenta, mas abajo aparece una mas rapida
 function printTable(gifts) {
     let maxItemGift = "Gift";
     let maxItemQuantity = "Quantity";
 
     gifts.map((gift) => {
-        if(gift.name.length > maxItemGift.length){
+        if (gift.name.length > maxItemGift.length) {
             maxItemGift = gift.name;
         }
-        if(String(gift.quantity).length > maxItemQuantity.length){
-            maxItemQuantity = String(gift.quantity)
-        }    
-    })
+        if (String(gift.quantity).length > maxItemQuantity.length) {
+            maxItemQuantity = String(gift.quantity);
+        }
+    });
 
-    let topBorder = "+".repeat((maxItemGift.length + maxItemQuantity.length) + 7)
-    let bottomBorder = "*".repeat((maxItemGift.length + maxItemQuantity.length) + 7)
-    let titles = `| Gift${" ".repeat(maxItemGift.length - 4)} | Quantity${" ".repeat(maxItemQuantity.length - 8)} |`;
-    let separator = `| ${"-".repeat(maxItemGift.length)} | ${"-".repeat(maxItemQuantity.length)} |`
-    let heading = `${topBorder}\n${titles}\n${separator}`
+    let topBorder = "+".repeat(maxItemGift.length + maxItemQuantity.length + 7);
+    let bottomBorder = "*".repeat(
+        maxItemGift.length + maxItemQuantity.length + 7
+    );
+    let titles = `| Gift${" ".repeat(
+        maxItemGift.length - 4
+    )} | Quantity${" ".repeat(maxItemQuantity.length - 8)} |`;
+    let separator = `| ${"-".repeat(maxItemGift.length)} | ${"-".repeat(
+        maxItemQuantity.length
+    )} |`;
+    let heading = `${topBorder}\n${titles}\n${separator}`;
 
     let body = "";
 
-    for(let i = 0; i < gifts.length; i++) {
+    for (let i = 0; i < gifts.length; i++) {
         let name = gifts[i].name;
         let quantity = gifts[i].quantity;
-        let line = `| ${name}${" ".repeat(maxItemGift.length - name.length)} | ${quantity}${" ".repeat(maxItemQuantity.length - String(quantity).length)} |`
+        let line = `| ${name}${" ".repeat(
+            maxItemGift.length - name.length
+        )} | ${quantity}${" ".repeat(
+            maxItemQuantity.length - String(quantity).length
+        )} |`;
 
-        if(i + 1 != gifts.length){
+        if (i + 1 != gifts.length) {
             line += "\n";
         }
         body += line;
     }
 
-    let all  = `${heading}\n${body}\n${bottomBorder}`;
+    let all = `${heading}\n${body}\n${bottomBorder}`;
 
     return all;
 }
@@ -54,10 +65,12 @@ console.log(
 //   | Book | 3        |
 //   *******************
 //   Otro ejemplo donde se puede ver que la tabla siempre usa sólo el espacio justo dependiendo de la longitud de los nombres de los regalos y de las cantidades.
-console.log(printTable([
-    { name: "PlayStation 5", quantity: 9234782374892 },
-    { name: "Book Learn Web Dev", quantity: 23531 },
-]));
+console.log(
+    printTable([
+        { name: "PlayStation 5", quantity: 9234782374892 },
+        { name: "Book Learn Web Dev", quantity: 23531 },
+    ])
+);
 //   ++++++++++++++++++++++++++++++++++++++
 //   | Gift               | Quantity      |
 //   | ------------------ | ------------- |
@@ -74,3 +87,33 @@ console.log(printTable([
 //   Adapta la tabla a la longitud de los nombres de los regalos y de las cantidades o los títulos de las columnas.
 //   No hace falta que ordenes los resultados.
 //   La tabla no termina con salto de línea.
+
+
+// ----------------------------------------------------------------
+// Funcion mas rapida
+// function printTable(gifts) {
+//     gifts = gifts.map((x) => [x.name, "" + x.quantity]);
+//     gifts.unshift(["Gift", "Quantity"]);
+
+//     let lengthGift = Math.max(...gifts.map((x) => x[0].length));
+//     let lengthQuantity = Math.max(...gifts.map((x) => x[1].length));
+
+//     gifts.splice(1, 0, ["-".repeat(lengthGift), "-".repeat(lengthQuantity)]);
+
+//     let tables = gifts.reduce(
+//         (str, x) =>
+//             str +
+//             "| " +
+//             x[0].padEnd(lengthGift) +
+//             " | " +
+//             x[1].padEnd(lengthQuantity) +
+//             " |" +
+//             "\n",
+//         ""
+//     );
+
+//     let top = "+".repeat(lengthGift + lengthQuantity + 7) + "\n";
+//     let bottom = "*".repeat(lengthGift + lengthQuantity + 7);
+
+//     return top + tables + bottom;
+// }
