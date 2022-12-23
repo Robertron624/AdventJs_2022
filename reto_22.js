6093ab8ad6f8cbf54256bba26a3b896cc85b552a
Roberto Ramirez <robert.624@hotmail.com>
2022-12-23 10:11:30 -0500

    reto 22 finished

--- reto_22.js
+++ reto_22.js
@@ -1,28 +1,26 @@
 function checkStepNumbers(systemNames, stepNumbers) {
 
-    let myArr = [];
+    let myObj = {};
 
-    const isInArray = (name, arr)=>{
-        for(let i=0; i<arr.length; i++){
-            if(arr[i][0] == name){
-                return true;
-            }
-        }
-        return false;
-    }
+    for(let index = 0; index < systemNames.length; index++){
+        let systemName = systemNames[index];
 
-    for(let i = 0; i < systemNames.length; i++){
-        console.log("iteacion -> ", i +1)
-        if(isInArray(systemNames[i], myArr)){
-            myArr[i][1].push(stepNumbers[i]);
+        // if systemName is not inside the myObj object then added with 
+        // value an array with corresponding stepnumber inside
+        if(!(Object.keys(myObj).includes(systemName))){
+            myObj[systemName] = [stepNumbers[index]];
         }
         else{
-            myArr.push([systemNames[i], stepNumbers[i]]);
+            // Add the step number i to the system name array but before check if the current number 
+            // is lower than the previous
+            if(stepNumbers[index] <  myObj[systemName]){
+                return false;
+            } 
+            myObj[systemName].push(stepNumbers[index]);
         }
     }
 
-
-    return myArr;
+    return true;
 }
 
 // Verifica que todas las secuencias independientes de sistemas de iluminación navideña estén en orden estrictamente creciente. Tenemos dos arrays: systemNames y stepNumbers.
@@ -36,7 +34,7 @@ function checkStepNumbers(systemNames, stepNumbers) {
 const systemNames = ["tree_1", "tree_2", "house", "tree_1", "tree_2", "house"]
 const stepNumbers = [1, 33, 10, 2, 44, 20]
 
-checkStepNumbers(systemNames, stepNumbers) // => true
+console.log(checkStepNumbers(systemNames, stepNumbers)) // => true
 
 // tree_1 tiene los pasos: [1, 2]
 // tree_2 tiene los pasos: [33, 44]
@@ -44,7 +42,7 @@ checkStepNumbers(systemNames, stepNumbers) // => true
 
 // true: Los pasos de cada sistema están en orden estrictamente creciente
 
-checkStepNumbers(["tree_1", "tree_1", "house"], [2, 1, 10]) // => false
+console.log(checkStepNumbers(["tree_1", "tree_1", "house"], [2, 1, 10])) // => false
 
 // tree_1 tiene los pasos: [2, 1]
 // house tiene los pasos: [10]
